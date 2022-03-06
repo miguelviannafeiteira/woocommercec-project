e892d93e256d9d085c3a40129090c38eac7b3e6b
Miguel Vianna <email@gmail.com>
2022-03-06 16:49:49 -0300

    slide na home adicionado

--- page-home.php
+++ page-home.php
@@ -2,8 +2,50 @@
 // Template name: Home
 get_header();?>
 
+
+<?php
+
+$products_slide = wc_get_products([
+'limit' => '4',
+'tag'=> ['slide'],
+]);
+
+function format_products($products){
+  $products_final = [];
+  foreach($products as $product){
+    $products_final[]=[
+      'name'=>  $product->get_name(),
+      'price'=> $product->get_price_html(),
+      'link'=> $product->get_permalink(),
+      'img'=>  wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+    ];
+  }
+  return $products_final;
+}
+
+$slide  = format_products($products_slide);
+?>
+
 <?php if(have_posts()) { while(have_posts()) { the_post()?>
 
-  <main><?php the_content()?></main>
-  <?php } } ?>
+
+<section class="slide-wrapper">
+  <ul class="slide">
+    <?php foreach($slide as $product){?>
+    <li class="slide-item">
+      <img src="<?=$product['img']?>;" alt="<?= $product['name'];?>"> 
+      <div class="slide-info">
+        <span class="slide-price"><?= $product['price'];?></span>
+        <h2 class="slide-name"><?= $product['name'];?></h2>
+        <a class="slide-link"href="<?= $product['link'];?>">Product</a>  
+      </div>
+  </li>
+    <?php } ?>
+  </ul>
+</section>
+
+
+
+
+<?php } } ?>
 <?php get_footer();?>
