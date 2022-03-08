b56ecdca7ed02da8e22d236a411954129e1432da
Miguel Vianna <email@gmail.com>
2022-03-07 21:28:49 -0300

    categoria adicionado na home

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once get_template_directory() . '/cmb2/load.php';
+
 register_nav_menus([
   'categorias'=>'Categorias'
   ]);
@@ -9,7 +12,7 @@ function helo_add_woocommerce_support(){
 }
 add_action('after_setup_theme','helo_add_woocommerce_support');
 
-// pusando o style.css
+// puxando o style.css
 // melhor q  puxar no header
 // get_template_directory_uri() . '/style.css pra pegar a pasta
 // enqueue pra colocar na fila
@@ -26,7 +29,27 @@ function helo_custom_images(){
 add_action('after_setup_theme', 'helo_custom_images');
 
 function helo_loop_shop_per_page(){
-  return 3;
+  return 5;
 }
-add_filter('loop_shop_per_page','helo_loop_shop_per_page')
+add_filter('loop_shop_per_page','helo_loop_shop_per_page');
+function helo_product_list($products){
+?>
+  <ul class="products-list">
+    <?php foreach($products as $product) {?>
+      <li class="product-item">
+        <a href="<?= $product['link']?>">
+          <div class="product-info">
+            <img src="<?= $product['img']?>" alt="<?= $product['name']?>">
+            <h2><?= $product['name']?> - <span><?= $product['price']?></span></h2>
+          </div>
+          <div class="product-overlay">
+            <span class="btn-link">Ver Mais</span>
+          </div>
+        </a>  
+    </li>
+    <?php } ?>
+  </ul>
+<?php
+};
+
 ?>
--- page-home.php
+++ page-home.php
@@ -1,29 +1,59 @@
 <?php
 // Template name: Home
 get_header();?>
-
-
 <?php
-
-$products_slide = wc_get_products([
-'limit' => '4',
-'tag'=> ['slide'],
-]);
-
-function format_products($products){
+function format_products($products, $img_size){
   $products_final = [];
   foreach($products as $product){
     $products_final[]=[
       'name'=>  $product->get_name(),
       'price'=> $product->get_price_html(),
       'link'=> $product->get_permalink(),
-      'img'=>  wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+      'img'=>  wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
     ];
   }
   return $products_final;
 }
+$products_slide = wc_get_products([
+'limit' => 5,
+'tag'=> ['slide'],
+]);
+$products_new = wc_get_products([
+  'limit' => 6,
+  'orderby'=> 'date',
+  'order'=>'DESC'
+]);
+$products_sales = wc_get_products([
+  'limit' => 6,
+  'meta_key'=> 'total_sales',
+  'orderby'=> 'meta_value_num',
+  'order'=>'DESC'
+]);
+$data=[];
 
-$slide  = format_products($products_slide);
+$data['slide']  = format_products($products_slide, 'slide');
+$data['releases']  = format_products($products_new, 'medium');
+$data['sales']  = format_products($products_sales, 'medium');
+
+$home_id = get_the_ID(); // pega o ID da pagina atual
+$categoria_esquerda = get_post_meta($home_id, 'categoria_esquerda', true);
+$categoria_direita = get_post_meta($home_id, 'categoria_direita', true);
+
+function get_product_category_data($category) {
+  $cat = get_term_by('slug', $category, 'product_cat');
+  $cat_id = $cat->term_id;
+  $img_id = get_term_meta($cat_id, 'thumbnail_id', true);
+  return [
+    'name' => $cat->name,
+    'id'=> $cat_id,
+    'link' => get_term_link($cat_id, 'product_cat' ),
+    'img' =>  wp_get_attachment_image_src($img_id, 'slide')[0]
+  ];
+}
+
+$data['categorias'][$categoria_esquerda] = get_product_category_data($categoria_esquerda);
+$data['categorias'][$categoria_direita] = get_product_category_data($categoria_direita);;
+
 ?>
 
 <?php if(have_posts()) { while(have_posts()) { the_post()?>
@@ -31,19 +61,39 @@ $slide  = format_products($products_slide);
 
 <section class="slide-wrapper">
   <ul class="slide">
-    <?php foreach($slide as $product){?>
+    <?php foreach($data['slide'] as $product){?>
     <li class="slide-item">
       <img src="<?=$product['img']?>;" alt="<?= $product['name'];?>"> 
       <div class="slide-info">
         <span class="slide-price"><?= $product['price'];?></span>
         <h2 class="slide-name"><?= $product['name'];?></h2>
-        <a class="slide-link"href="<?= $product['link'];?>">Product</a>  
+        <a class="btn-link"href="<?= $product['link'];?>">Product</a>  
       </div>
   </li>
     <?php } ?>
   </ul>
 </section>
 
+<section class="container">
+  <h1 class="subtitle">Releases</h1>
+  <?php helo_product_list($data['releases'])?>
+</section>
+
+
+<section class="categorias-home">
+    <?php foreach ($data['categorias'] as $categoria) { ?>
+      <a href="<?= $categoria['link']?>">
+        <img src="<?= $categoria['img']?>" alt="<?= $categoria['name']?>">
+        <span class="btn-link link-categoria"><?= $categoria['name']?></span>
+      </a>
+    <?php } ?>
+  </section>
+
+
+<section class="container">
+  <h1 class="subtitle">Best sellers</h1>
+  <?php helo_product_list($data['sales'])?>
+</section>
 
 
 
