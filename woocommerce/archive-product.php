7a8cd1da3a4c14a27fd3f1e8c316108c403788e6
Miguel Vianna <email@gmail.com>
2022-03-08 20:24:11 -0300

    filtros adicionados

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -18,10 +18,35 @@ $data['products'] = format_products($products);
 
 <article class="container products-archive">
   <nav class="filtros">
-    <h2>Filtors</h2>
+    <div class="filtro">
+      <h3 class="filtro-titulo">Categories</h3>
+      <?php
+        wp_nav_menu([
+          'menu'=>'categorias-interna',
+          'menu_class'=>'filtro-cat',
+          'container'=>false,
+        ]);
+      ?>
+    </div>
+
+    <div class="filtro">
+      <h3 class="filtro-titulo">Filter by price</h3>
+      <form class="filtro-preco">
+        <div>
+          <label for="min_price">De R$</label>
+          <input required type="text" name="min_price" id="min_price" value="<?php $_GET['min_price']?>">
+        </div>
+        <div>
+          <label for="max_price">Até R$</label>
+          <input required type="text" name="max_price" id="max_price" value="<?php $_GET['max_price']?>">
+        </div>
+        <button type="submit">Filter</button>
+      </form>
+    </div>
   </nav>
   <main>
     <?php if($data['products'][0]) { ?>
+      <?php woocommerce_catalog_ordering();?>
     <?php helo_product_list($data['products']);?>
     <?= get_the_posts_pagination()?>
     <?php } else {?>
